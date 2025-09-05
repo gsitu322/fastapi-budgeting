7cc49457dab42ca434dbdcced044a2b8428defbb
Gordon Situ <gsitu322@gmail.com>
2025-09-04 22:12:05 -0700

    Added models

---
+++ app/models.py
@@ -0,0 +1,49 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, Float, ForeignKey, Enum
+from .database import Base
+from .core.enums import CategoryType
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String, unique=True)
+    hashed_password = Column(String)
+    first_name = Column(String)
+    last_name = Column(String)
+    is_active = Column(Boolean)
+    create_dt = Column(DateTime)
+    update_dt = Column(DateTime)
+
+
+class Source(Base):
+    __tablename__ = 'sources'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    create_dt = Column(DateTime)
+    update_dt = Column(DateTime)
+
+
+class Category(Base):
+    __tablename__ = 'categories'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String)
+    type = Column(Enum(CategoryType), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    create_dt = Column(DateTime)
+    update_dt = Column(DateTime)
+
+
+class Transaction(Base):
+    __tablename__ = 'transactions'
+
+    id = Column(Integer, primary_key=True)
+    amount = Column(Float)
+    date = Column(DateTime)
+    description = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    notes = Column(String)
+    create_dt = Column(DateTime)
+    update_dt = Column(DateTime)
